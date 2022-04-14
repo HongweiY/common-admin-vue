4d81b9bc9c861d0c6cbd8c9a2ced134a4b917bc4
YHW <ymfsder@163.com>
2022-04-14 16:41:55 +0800

    eslit规则修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,108 +1,24 @@
 module.exports = {
-    root: true,
-    env: {
-        es6: true,
-        browser: true,
-        node: true
-    },
-    extends: [
-        'plugin:import/recommended',
-        'plugin:markdown/recommended',
-        'plugin:vue/vue3-recommended',
-        'plugin:vue/essential'
-    ],
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module'
-    },
-    plugins: [
-        'vue'
-    ],
-    rules: {
-        'no-console': ['warn', { allow: ['error'] }],
-        'no-debugger': 'warn',
-        'no-constant-condition': ['error', { checkLoops: false }],
-        'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
-        'no-return-await': 'error',
-        'no-var': 'error',
-        'no-empty': ['error', { allowEmptyCatch: true }],
-        'prefer-const': [
-            'warn',
-            { destructuring: 'all', ignoreReadBeforeAssign: true }
-        ],
-        'prefer-arrow-callback': [
-            'error',
-            { allowNamedFunctions: false, allowUnboundThis: true }
-        ],
-        'object-shorthand': [
-            'error',
-            'always',
-            { ignoreConstructors: false, avoidQuotes: true }
-        ],
-        'prefer-rest-params': 'error',
-        'prefer-spread': 'error',
-        'prefer-template': 'error',
-
-        'no-redeclare': 'off',
-        // vue
-        'vue/no-v-html': 'off',
-        'vue/require-default-prop': 'off',
-        'vue/require-explicit-emits': 'off',
-        'vue/multi-word-component-names': 'off',
-        'vue/html-self-closing': [
-            'error',
-            {
-                html: {
-                    void: 'always',
-                    normal: 'always',
-                    component: 'always'
-                },
-                svg: 'always',
-                math: 'always'
-            }
-        ],
-
-        // import
-        'import/first': 'error',
-        'import/no-duplicates': 'error',
-        'import/order': [
-            'error',
-            {
-                groups: [
-                    'builtin',
-                    'external',
-                    'internal',
-                    'parent',
-                    'sibling',
-                    'index',
-                    'object',
-                    'type'
-                ],
-
-                pathGroups: [
-                    {
-                        pattern: 'vue',
-                        group: 'external',
-                        position: 'before'
-                    },
-                    {
-                        pattern: '@vue/**',
-                        group: 'external',
-                        position: 'before'
-                    },
-                    {
-                        pattern: '@element-plus/**',
-                        group: 'internal'
-                    }
-                ],
-                pathGroupsExcludedImportTypes: ['type']
-            }
-        ],
-        'import/no-unresolved': 'off',
-        'import/namespace': 'off',
-        'import/default': 'off',
-        'import/no-named-as-default': 'off',
-        'import/no-named-as-default-member': 'off',
-        'import/named': 'off'
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'plugin:import/recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    // parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    // '@typescript-eslint'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'prefer-destructuring': ['error', { object: true, array: true }],
+    'no-param-reassign': ['error', { props: false }],
+  },
 }
---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 120,
+  // 箭头函数只有一个参数的时候可以忽略括号
+  arrowParens: 'avoid',
+  // 括号内部不要出现空格
+  bracketSpacing: true,
+  // 行结束符使用 Unix 格式
+  endOfLine: 'lf',
+  // 换行方式
+  proseWrap: 'preserve',
+  // 分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 缩进
+  tabWidth: 2,
+  // 使用 tab 缩进
+  useTabs: false,
+}
