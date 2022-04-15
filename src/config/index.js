dfd89ddf304c9327312d752831e46ecacbda78bc
YHW <ymfsder@163.com>
2022-04-15 10:56:10 +0800

    eslit规则修改

--- src/config/index.js
+++ src/config/index.js
@@ -2,26 +2,25 @@
  * 环境配置封装
  */
 const env = import.meta.env.MODE || 'prod'
-console.log(env)
+
 const EnvConfig = {
-    dev: {
-        baseApi: '/api',
-        mockApi: 'https://www.fastmock.site/mock/fee937e1715028d2d12089b393e7790a/api'
-    },
-    test: {
-        baseApi: '/',
-        mockApi: 'https://www.fastmock.site/mock/fee937e1715028d2d12089b393e7790a/api'
-    },
-    prod: {
-        baseApi: '/',
-        mockApi: ''
-    }
+  dev: {
+    baseApi: '/api',
+    mockApi: 'https://www.fastmock.site/mock/fee937e1715028d2d12089b393e7790a/api',
+  },
+  test: {
+    baseApi: '/',
+    mockApi: 'https://www.fastmock.site/mock/fee937e1715028d2d12089b393e7790a/api',
+  },
+  prod: {
+    baseApi: '/',
+    mockApi: '',
+  },
 }
 
-
 export default {
-    env: 'dev',
-    mock: false,
-    namespace: 'admin',
-    ...EnvConfig[env]
+  env: 'dev',
+  mock: false,
+  namespace: 'admin',
+  ...EnvConfig[env],
 }
